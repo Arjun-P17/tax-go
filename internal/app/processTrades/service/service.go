37618515e60585163d4483d345c1d26deeb522c9
Arjun <arjun.pushpavannan@gmail.com>
2024-03-23 17:04:55 +1100

    add fifo test

--- internal/app/processTrades/service/service.go
+++ internal/app/processTrades/service/service.go
@@ -1,21 +1,14 @@
 package service
 
 import (
-	"context"
-
 	"github.com/Arjun-P17/tax-go/internal/repository"
 )
 
-type serviceInterface interface {
-	ProcessTrades(ctx context.Context) error
-}
-
 type Service struct {
-	serviceInterface
 	repository repository.RepositoryInterface
 }
 
-func NewService(repo *repository.Repository) *Service {
+func NewService(repo repository.RepositoryInterface) *Service {
 	return &Service{
 		repository: repo,
 	}
