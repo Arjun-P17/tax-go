bc79732bad5299c587ed184ce7aec662a50464a6
Arjun-ANZ <137134989+Arjun-ANZ@users.noreply.github.com>
2024-03-23 17:33:26 +1100

    edit testutils

--- internal/testutils/file.go
+++ internal/testutils/file.go
@@ -4,15 +4,27 @@ import (
 	"encoding/json"
 	"os"
 
+	"github.com/Arjun-P17/tax-go/internal/app/parseTrades/service"
 	"github.com/Arjun-P17/tax-go/internal/repository"
 )
 
-func SaveToFile(transactions []*repository.Transaction, filename string) error {
+func SaveToFile(inputcsv string, outputfile string) error {
+	file, err := os.Open(inputcsv)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	transactions, err := service.ParseTransactions(nil)
+	if err != nil {
+		return err
+	}
+
 	data, err := json.Marshal(transactions)
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(filename, data, 0o644)
+	err = os.WriteFile(outputfile, data, 0o644)
 	if err != nil {
 		return err
 	}
