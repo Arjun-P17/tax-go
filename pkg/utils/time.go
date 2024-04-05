be7caf63f41a2e63ea4414c6f8b0175d0cc28a49
Arjun-ANZ <137134989+Arjun-ANZ@users.noreply.github.com>
2024-04-06 05:24:18 +1100

    add GetFYYearString

--- pkg/utils/time.go
+++ pkg/utils/time.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -19,4 +20,16 @@ func TimeToString(t time.Time) string {
 
 func IsOneYearGreaterThan(date1, date2 time.Time) bool {
 	return date1.AddDate(1, 0, 0).Before(date2)
+}
+
+// GetFYYearString returns australian tax year in format YYYY-YYYY
+func GetFYYearString(date time.Time) string {
+	year, month, _ := date.Date()
+	startYear := year
+	if month < 7 {
+		startYear = year - 1
+	}
+	endYear := startYear + 1
+	taxYear := fmt.Sprintf("%d-%d", startYear, endYear)
+	return taxYear
 }
